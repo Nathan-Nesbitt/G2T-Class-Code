6e6d867a0a7a24728f827b96a87127592692fa5b
Nathan Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-02-11 19:23:10 -0800

    Added Example Form

--- node/index.js
+++ node/index.js
@@ -7,6 +7,9 @@ const app = express();
 // Import the EHS Library into express //
 app.set('view engine', "ejs");
 
+// Handle Post Request Body Data //
+app.use(express.urlencoded())
+
 // Set a port number //
 const port = 3000;
 
@@ -35,8 +38,19 @@ app.get("/manuscript", (req, res) => {
 
 // EJS Example //
 app.get("/favorite_foods", (req, res) => {
-    var fav_foods = ["Taco", "Steak", "Pizza", "Cheese Slice", "Travis Scott Burger"];
+    var fav_foods = ["Taco", "Steak", "Pizza", "Cheese Slice", "Travis Scott Burger", "Ice", "Fruit by the Foot"];
     res.render("favfoods.ejs", {foods: fav_foods})
+})
+
+// Form Example //
+app.get("/my_form", (req, res) => {
+    res.render("form");
+})
+
+app.post("/my_form", (req, res) => {
+    console.log(req.body.name);
+    console.log(req.body.password);
+    res.send("CREATED ACCOUNT");
 })
 
 // Start the server //
