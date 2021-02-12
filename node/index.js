2b43db02a8bfb7d2bca9d5bd5f60edb17dfd73f8
Nathan Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-02-11 19:02:18 -0800

    Added Some EJS

--- node/index.js
+++ node/index.js
@@ -4,6 +4,9 @@ const express = require("express");
 // Creates an express application //
 const app = express();
 
+// Import the EHS Library into express //
+app.set('view engine', "ejs");
+
 // Set a port number //
 const port = 3000;
 
@@ -23,6 +26,17 @@ app.get("/homepage", (req, res) => {
 // Creates a /gator endpoint //
 app.get("/gator", (req, res) => {
     res.send("🐊")
+})
+
+// Nathan's Manuscript //
+app.get("/manuscript", (req, res) => {
+    res.render("manuscript.ejs")
+})
+
+// EJS Example //
+app.get("favorite_foods", (req, res) => {
+    var fav_foods = ["Taco", "Steak", "Pizza", "Cheese Slice", "Travis Scott Burger"];
+    res.render("favfoods.ejs", {foods: fav_foods})
 })
 
 // Start the server //
