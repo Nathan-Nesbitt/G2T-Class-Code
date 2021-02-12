4f70e5b7d353f828db84c827f3e25bd10031ee92
Nathan Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-02-11 18:27:44 -0800

    Added Basic Server Code

--- node/index.js
+++ node/index.js
@@ -3,3 +3,14 @@ const express = require("express");
 
 // Creates an express application //
 const app = express();
+
+// Set a port number //
+const port = 3000;
+
+// Set an endpoint //
+app.get("/", (request, response) => {
+    response.send("HELLO WORLD!");
+})
+
+// Start the server //
+app.listen(port);
