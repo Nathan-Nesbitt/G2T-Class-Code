4a4d51106ae86783f5fbc1549ee496d7c83467f2
Nathan Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-02-16 19:20:58 -0800

    Added Select Statements

--- sql/commands.sql
+++ sql/commands.sql
@@ -25,3 +25,24 @@ INSERT INTO people(SIN, name) VALUES (111111112, "Nisa");
 INSERT INTO people(SIN, name) VALUES (111111113, "Cai");
 INSERT INTO people(SIN, name) VALUES (111111114, "Cai");
 INSERT INTO people(SIN, name) VALUES (111111115, "Lucas");
+
+/* View all of the people that we inserted into the table/database */
+SELECT * FROM people;
+
+/* Select all people with the name 'cai' */
+SELECT * FROM people WHERE name = 'Cai';
+
+/* Select the name of the person with the SIN 111111112 */
+SELECT name FROM people WHERE SIN = 111111112;
+
+/* Select all people whose names start with 'N' */
+SELECT name, SIN FROM people WHERE name LIKE "N%";
+
+/* Select all the people whose second last letter of their name is a */
+SELECT name, SIN FROM people WHERE name LIKE "%a_";
+
+/* Select all the people whose second letter of their name is n */
+SELECT name, SIN FROM people WHERE name LIKE "_n%";
+
+/* Select all the people whose first and last letter of their name is n */
+SELECT name, SIN FROM people WHERE name LIKE "n%n";
